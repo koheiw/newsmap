a35c8dcd599a1fb57c738f385819e144cc0baaf1
Kohei Watanabe <watanabe.kohei@gmail.com>
2019-11-07 13:23:37 +0100

    Add tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -6,7 +6,7 @@ require(testthat)
 
 check_character <- function(x) {
     x <- unlist(x)
-    expect_true(all(!stri_detect_regex(x, "[^\\p{L}'*?\\-\\u30fb ]")))
+    expect_true(all(!stri_detect_regex(x, "[^\\p{L}'\"*?\\-\\u30fb ]")))
 }
 
 test_that("test that yaml do not contain illegal letters", {
@@ -16,6 +16,7 @@ test_that("test that yaml do not contain illegal letters", {
     check_character(data_dictionary_newsmap_es)
     check_character(data_dictionary_newsmap_ja)
     check_character(data_dictionary_newsmap_ru)
+    check_character(data_dictionary_newsmap_he)
     check_character(data_dictionary_newsmap_zh_cn)
     check_character(data_dictionary_newsmap_zh_tw)
 })
@@ -29,6 +30,7 @@ test_that("test that data file is created correctly", {
     expect_equal(length(names(data_dictionary_newsmap_ja)), 5)
     expect_equal(length(names(data_dictionary_newsmap_ru)), 5)
     expect_equal(length(names(data_dictionary_newsmap_it)), 5)
+    expect_equal(length(names(data_dictionary_newsmap_he)), 5)
     expect_equal(length(names(data_dictionary_newsmap_zh_cn)), 5)
     expect_equal(length(names(data_dictionary_newsmap_zh_tw)), 5)
 })
@@ -42,6 +44,7 @@ test_that("test that dictionaries have the same countries", {
     ja <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_ja, 3))
     ru <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_ru, 3))
     it <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_it, 3))
+    he <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_he, 3))
     zh_cn <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_zh_cn, 3))
     zh_tw <- names(quanteda:::flatten_dictionary(data_dictionary_newsmap_zh_tw, 3))
 
@@ -51,6 +54,7 @@ test_that("test that dictionaries have the same countries", {
     expect_true(identical(en, ja))
     expect_true(identical(en, ru))
     expect_true(identical(en, it))
+    expect_true(identical(en, he))
     expect_true(identical(en, zh_cn))
     expect_true(identical(en, zh_tw))
 })
